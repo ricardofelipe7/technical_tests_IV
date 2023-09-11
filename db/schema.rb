d5ec2131a26bc631e650678b844e0ccf214b8dc4
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-11 17:21:10 -0300

    refactor: adding column in table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_172043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "csv_imports", force: :cascade do |t|
+    t.string "show_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

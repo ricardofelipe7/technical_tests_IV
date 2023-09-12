ab3805278281f98af20fb6a8600918db40f36765
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-12 17:12:33 -0300

    test: model and controller adding.

--- test/controllers/csv_imports_controller_test.rb
+++ test/controllers/csv_imports_controller_test.rb
@@ -1,38 +1,8 @@
 require "test_helper"
 
 class CsvImportsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @csv_import = csv_imports(:one)
-  end
-
-  test "should get index" do
-    get csv_imports_url, as: :json
+  test "should get create" do
+    get csv_imports_create_url
     assert_response :success
-  end
-
-  test "should create csv_import" do
-    assert_difference("CsvImport.count") do
-      post csv_imports_url, params: { csv_import: {  } }, as: :json
-    end
-
-    assert_response :created
-  end
-
-  test "should show csv_import" do
-    get csv_import_url(@csv_import), as: :json
-    assert_response :success
-  end
-
-  test "should update csv_import" do
-    patch csv_import_url(@csv_import), params: { csv_import: {  } }, as: :json
-    assert_response :success
-  end
-
-  test "should destroy csv_import" do
-    assert_difference("CsvImport.count", -1) do
-      delete csv_import_url(@csv_import), as: :json
-    end
-
-    assert_response :no_content
   end
 end

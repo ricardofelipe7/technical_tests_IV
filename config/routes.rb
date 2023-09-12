73285eab7b4ade7894ac1d810ec89d93de40d3f7
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-12 17:13:07 -0300

    refactor: route csv

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :csv_imports
+  post 'csv_imports/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

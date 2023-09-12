ae2d64d196dc2ba86958b634cf499812f20c5ff2
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-12 17:10:45 -0300

    refactor: remove files

--- app/models/csv_import.rb
+++
@@ -1,3 +0,0 @@
-class CsvImport < ApplicationRecord
-
-end
--- db/migrate/20230911172043_create_csv_imports.rb
+++
@@ -1,8 +0,0 @@
-class CreateCsvImports < ActiveRecord::Migration[7.0]
-  def change
-    create_table :csv_imports do |t|
-      t.string 'show_id'
-      t.timestamps
-    end
-  end
-end
